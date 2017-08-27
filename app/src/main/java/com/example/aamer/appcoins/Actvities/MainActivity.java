8a2109506efe58cb8a55af3dc5b9af3da17d07de
Amr M.Saidam <sc.amr.2017@gmail.com>
2017-08-27 15:55:28 +0300

    all coins have been fetched and viewed successfully

--- app/src/main/java/com/example/aamer/appcoins/Actvities/MainActivity.java
+++ app/src/main/java/com/example/aamer/appcoins/Actvities/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.aamer.appcoins.Actvities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.example.aamer.appcoins.Adapters.CoinsAdapter;
 import com.example.aamer.appcoins.Constants.Coins;
 import com.example.aamer.appcoins.Network.AllCoins;
 import com.example.aamer.appcoins.R;
@@ -13,11 +16,16 @@ import com.example.aamer.appcoins.Structure.VolleyCallback;
 import org.json.JSONArray;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.coins);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(llm);
         fillWithCoins();
     }
 
@@ -26,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void success(JSONArray jsonArray) {
                 try {
+                    recyclerView.setAdapter(new CoinsAdapter(MainActivity.this.getApplicationContext(), jsonArray));
                     Log.d("content", jsonArray.get(0).toString());
                 } catch (Exception exception) {
                     Toast.makeText(MainActivity.this.getApplicationContext(), "Exception", Toast.LENGTH_LONG).show();
---
+++ app/src/main/java/com/example/aamer/appcoins/Adapters/CoinsAdapter.java
@@ -0,0 +1,70 @@
+package com.example.aamer.appcoins.Adapters;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.aamer.appcoins.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by aamer on 8/27/2017.
+ */
+
+public class CoinsAdapter extends RecyclerView.Adapter<CoinsAdapter.ViewHolder> {
+
+    private Context context;
+    private JSONArray coins;
+
+    public CoinsAdapter(Context context, JSONArray coins) {
+        this.context = context;
+        this.coins = coins;
+
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.coin_cell, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        try {
+            JSONObject coin = coins.getJSONObject(position);
+            holder.coinName.setText(coin.getString("id"));
+            holder.price.setText(coin.getString("price"));
+            holder.tradeDate.setText(coin.getString("latest_trade"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return coins.length();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        public AppCompatTextView coinName;
+        public AppCompatTextView price;
+        public AppCompatTextView tradeDate;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            coinName = (AppCompatTextView) itemView.findViewById(R.id.coin_name);
+            price = (AppCompatTextView) itemView.findViewById(R.id.trade_date);
+            tradeDate = (AppCompatTextView) itemView.findViewById(R.id.price);
+
+        }
+    }
+}
